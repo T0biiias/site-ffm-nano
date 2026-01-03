freifunkMUC/site-ffm

modules: add ebpf-clat test package

image-customization.lua
@@ -24,6 +24,7 @@ packages{
     'ffbs-mesh-vpn-parker',
     'ffbs-wireguard-respondd',
     'ffda-node-whisperer',
+    'ffmuc-ebpfs-clat',
     'ffmuc-ipv6-ra-filter',
     'ffmuc-mesh-vpn-wireguard-vxlan',
     '-gluon-ebtables-limit-arp',

T0biiias/site-ffm-nano

image-customization.lua
@@ -24,11 +24,11 @@ packages{
     'ffbs-mesh-vpn-parker',
     'ffbs-wireguard-respondd',
     'ffda-node-whisperer',
+    'ffmuc-ebpf-clat',
     'ffmuc-ipv6-ra-filter',
     'ffmuc-mesh-vpn-wireguard-vxlan',
     '-gluon-ebtables-limit-arp',
     'iwinfo',
-    'kmod-nat46',
     'respondd-module-airtime',
 }
 